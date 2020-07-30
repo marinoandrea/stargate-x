caadd596781d887d049b1e33f9b106c3592bfe9b
Andrea Marino <am.marinoandrea@gmail.com>
2020-07-30 10:04:46 +0200

    namespace imports fixed

--- reactome_graph/__init__.py
+++ reactome_graph/__init__.py
@@ -2,5 +2,5 @@ ENTITY, EVENT = 0, 1
 EDGES_QUERY = 'reactome_graph/queries/edges.cypher'
 PATHWAYS_QUERY = 'reactome_graph/queries/pathways.cypher'
 
-from reactome_graph.analysis import GraphAnalyzer  # noqa: F401, E402
 from reactome_graph.graph_builder import GraphBuilder  # noqa: F401, E402
+from reactome_graph.analysis import *  # noqa: F401, E402, F403
