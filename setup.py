3c12f4e22103c27c44eca637b20ef26ba0f26549
Andrea Marino <am.marinoandrea@gmail.com>
2020-11-11 10:32:24 +0100

    project: distribution files are up to date

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+
+setup(
+  name='reactome-graph',
+  packages=['reactome_graph'],
+  version='1.0',
+  description=(
+    '''
+    The package is a python implementation of a bipartite directed multigraph
+    extracted from the Reactome database.
+    Its underlying implementation uses `networkx`.
+    '''
+  ),
+  author='Andrea Marino',
+  author_email='am.marinoandrea@gmail.com',
+  url='https://github.com/marinoandrea/reactome-graph',
+  download_url='https://github.com/marinoandrea/reactome-graph/tarball/1.0',
+  keywords=['reactome', 'pathway', 'pathways', 'graph', 'bipartite'],
+  include_package_data=True,
+  install_requires=['networkx', 'neo4j', 'numpy', 'scipy'],
+  package_data={
+    'reactome_graph.data': 'reactome_graph/data/*.pickle',
+    'reactome_graph.queries': 'reactome_graph/queries/*.cypher',
+  }
+)
