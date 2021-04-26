851dac641450b60bc89d836054a2dfa086df95c9
Andrea Marino <am.marinoandrea@gmail.com>
2021-04-26 22:08:59 +0200

    [api] dataclasses are now public

--- reactome_graph/__init__.py
+++ reactome_graph/__init__.py
@@ -1,7 +1,10 @@
+from .data import Compartment, Pathway
 from .graph import ReactomeGraph
 from .species import Species
 
 __all__ = [
     'ReactomeGraph',
     'Species',
+    'Pathway',
+    'Compartment'
 ]
